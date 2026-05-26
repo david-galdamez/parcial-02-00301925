545ee92e62aa816474535a3204f85050da681710
David Galdamez <david.galdamez2014@gmail.com>
2026-05-25 18:03:24 -0600

    Primer commit del parcial 02

---
+++ src/main/java/com/parcial/parcial02/Parcial02Application.java
@@ -0,0 +1,13 @@
+package com.parcial.parcial02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcial02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parcial02Application.class, args);
+	}
+
+}
